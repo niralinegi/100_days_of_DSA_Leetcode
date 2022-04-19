eb8201337b57cfe5c1042ba2fc3a1fca20ce96e3
niralinegi <58590628+niralinegi@users.noreply.github.com>
2022-04-19 21:36:52 +0530

    Time: 47 ms (38.17%), Space: 13.8 MB (62.90%) - LeetHub

---
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        answer = []
+        self.preorder(root, answer)
+        return answer
+    
+    def preorder(self, root, answer):
+        if root is None:
+            return
+        
+        answer.append(root.val)
+        self.preorder(root.left, answer)
+        self.preorder(root.right, answer)
+        
+        
+        
