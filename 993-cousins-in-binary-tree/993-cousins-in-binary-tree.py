8a2ffe0b8f3123208d212f932814e78dd7d3121c
niralinegi <58590628+niralinegi@users.noreply.github.com>
2022-04-25 21:08:18 +0530

    Time: 60 ms (17.97%), Space: 13.8 MB (92.61%) - LeetHub

---
+++ 993-cousins-in-binary-tree/993-cousins-in-binary-tree.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isCousins(self, root: Optional[TreeNode], x: int, y: int) -> bool:
+        parents = [0, 0]
+        levels = [0, 0]
+        
+        self.findParentsAndLevel(root, TreeNode(0,0,0), 0, x, y, parents, levels)
+        
+        return (parents[0] != parents[1]) and (levels[0] == levels[1])
+    
+    def findParentsAndLevel(self, root, currentParent, currentLevel, x, y, parents, levels):
+        if root is None:
+            return
+        
+        if root.val == x:
+            parents[0] = currentParent.val
+            levels[0] = currentLevel
+            
+        if root.val == y:
+            parents[1] = currentParent.val
+            levels[1] = currentLevel
+            
+        self.findParentsAndLevel(root.left, root, currentLevel+1, x, y, parents, levels)
+        self.findParentsAndLevel(root.right, root, currentLevel+1, x, y, parents, levels)
+        
+        return
